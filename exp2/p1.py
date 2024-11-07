bda1701e98b7bf53415960fcf3c97ebaaa5ea1ab
nmrintel <sakedaisuki3.14@gmail.com>
2024-11-07 16:13:32 +0900

    pre-commit

--- exp2/p1.py
+++ exp2/p1.py
@@ -7,7 +7,8 @@ import sys
 data_path = Path(__file__).resolve().parent / "data"
 sys.path.append(Path(__file__).resolve().parent)
 
-def min_sq_err(x:np.ndarray,phi:callable,y:np.ndarray)->np.ndarray:
+
+def min_sq_err(x: np.ndarray, phi: callable, y: np.ndarray) -> np.ndarray:
     """最小二乗法
 
     Args:
@@ -18,35 +19,38 @@ def min_sq_err(x:np.ndarray,phi:callable,y:np.ndarray)->np.ndarray:
     Returns:
         np.ndarray: thetaの推定値
     """
-    
+
     data_size = x.shape[0]
-    N = (phi(x,0)).shape[1] 
+    N = (phi(x, 0)).shape[1]
     # φの返り値の次元が不明なので、0番目のxを用いて特定
-    
-    inv_mat:np.ndarray = np.zeros((N,N))
-    right_mat = np.zeros((N,N))
-    
+
+    inv_mat: np.ndarray = np.zeros((N, N))
+    right_mat = np.zeros((N, N))
+
     for row_idx in range(data_size):
-        inv_mat += (phi(x,row_idx).T) @ phi(x,row_idx)
-        right_mat += (phi(x,row_idx).T) @ y[row_idx]
-    
+        inv_mat += (phi(x, row_idx).T) @ phi(x, row_idx)
+        right_mat += (phi(x, row_idx).T) @ y[row_idx]
+
     theta_hat = np.linalg.inv(inv_mat) @ right_mat
     return theta_hat
 
-def conv_mat(x:np.ndarray,phi: callable,y:np.ndarray,theta: np.ndarray)->np.ndarray:
+
+def conv_mat(x: np.ndarray, phi: callable, y: np.ndarray, theta: np.ndarray) -> np.ndarray:
     data_size = x.shape[0]
     dim_theta = theta.shape[0]
-    
-    conv_mat = np.zeros((dim_theta,dim_theta))
+
+    conv_mat = np.zeros((dim_theta, dim_theta))
     for idx in range(dim_theta):
         conv_mat += (y[idx]-phi(idx)@theta)@(y[idx]-phi(idx)@theta).T
-    
+
     return conv_mat/(data_size-dim_theta)
 
-def conf_det(data: np.ndarray,theta:np.ndarray):
+
+def conf_det(data: np.ndarray, theta: np.ndarray):
     N = data.shape[0]
-    y_ave = np.mean(data[:,-1])
-    phi = lambda idx: np.array(data[idx,:-1]).reshape(1,-1)
+    y_ave = np.mean(data[:, -1])
+    def phi(idx): return np.array(data[idx, :-1]).reshape(1, -1)
+
 
 def main():
     # 1. Read data
@@ -55,26 +59,27 @@ def main():
         header=None
     )
     data = data.to_numpy()
-    deg_list = [2**k for k in np.arange(1,14,1)]
-    
-    def phi(x,idx):
-        return np.array(x[idx,0],x[idx,1]).reshape(1,-1)
-    
-    theta_process = np.ndarray((len(deg_list),2))
+    deg_list = [2**k for k in np.arange(1, 14, 1)]
+
+    def phi(x, idx):
+        return np.array(x[idx, 0], x[idx, 1]).reshape(1, -1)
+
+    theta_process = np.ndarray((len(deg_list), 2))
     print(theta_process.shape)
-    for idx,deg in enumerate(deg_list):
-        theta,_ = min_sq_err(data[:deg,:-1],phi,data[:deg,-1])
-        theta_process[idx,0] = theta[0]
-        theta_process[idx,1] = theta[1]
-    
+    for idx, deg in enumerate(deg_list):
+        theta, _ = min_sq_err(data[:deg, :-1], phi, data[:deg, -1])
+        theta_process[idx, 0] = theta[0]
+        theta_process[idx, 1] = theta[1]
+
     plt.xscale("log")
-    plt.plot(deg_list,theta_process[:,0])
+    plt.plot(deg_list, theta_process[:, 0])
     plt.savefig("1.png")
-    
+
     plt.clf()
-    plt.plot(deg_list,theta_process[:,1])
+    plt.plot(deg_list, theta_process[:, 1])
     plt.tight_layout()
     plt.savefig("2.png")
-    
+
+
 if __name__ == '__main__':
     main()
